aae7f402352d48451fc273021f6464263ce0d4f7
Aitor Angulo Salas <71783846+HERRERA99@users.noreply.github.com>
2025-02-27 11:05:55 +0100

    Fix sonnar

--- AndroidProject/app/build.gradle.kts
+++ AndroidProject/app/build.gradle.kts
@@ -7,15 +7,9 @@ plugins {
 
 sonar {
     properties {
-        property("sonar.host.url", "https://sonarcloud.io")
-        property("sonar.organization", "herrera99")
-        property("sonar.token", "b35b41c8b23c113fa7594a62df6624729a509b49")
         property("sonar.projectKey", "HERRERA99_TFG-AppTrackActividades")
-        property("sonar.projectName", "TFG-AppTrackActividades")
-
-        // I need this property to avoid the error where sonarqube does not close some files and
-        // prevents a clean afterwards
-        property("sonar.scm.disabled", true)
+        property("sonar.organization", "herrera99")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
 
